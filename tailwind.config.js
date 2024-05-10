f58b6e5bed5aebee2eba667c1bde7fc70ea2cbbf
raad <raadbouks@gmail.com>
2024-05-10 06:35:05 +0000

    changing background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,7 +24,7 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          DEFAULT: "#7D41E1",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
