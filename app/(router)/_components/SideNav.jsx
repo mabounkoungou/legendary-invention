dd777d3d9d22b3fa3ac3dc1dd804577416b956b2
raad <raadbouks@gmail.com>
2024-05-10 08:46:12 +0000

    implement sidebar components

--- app/(router)/_components/SideNav.jsx
+++ app/(router)/_components/SideNav.jsx
@@ -15,18 +15,28 @@ function SideNav() {
         },
         {
             id:3,
-            name:"Become a Tutor",
+            name:"Tutor",
             Icon:GraduationCap
         }
     ]
   return (
     <div className='p-5 bg-white 
     rounded-xl h-screen m-3 '>
-            <img src="/logo.svg" alt="logo" height={170} width={80}/>
-            <div>
+            <img src="/logo.svg" alt="logo" height={170} width={80} 
+            className='hover:animate-bounce mb-2 items-center ml-2'/>
+
+            <hr className='mt-6'></hr>
+            <div className='mt-8'>
             {menu.map((item,index) => (
-                <div key={index} className='flex gap-3'>
-                <item.Icon/>
+                <div key={index} className='group flex gap-3 
+                mt-2 p-3 text-[20px] 
+                items-center text-gray-500 
+                cursor-pointer hover:bg-primary
+                 hover:text-white
+                  hover:rounded-lg 
+                  transition-all ease-in-out duration-300
+                  '>
+                <item.Icon className='group-hover:animate-bounce'/>
                 <h2>{item.name}</h2>
                 </div>
               )  )}
