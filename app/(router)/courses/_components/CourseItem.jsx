cecb3959d5bdb821bd2e3057c1fc984bdeed4114
raad <raadbouks@gmail.com>
2024-05-12 12:03:34 +0000

    Displaying Courses

--- app/(router)/courses/_components/CourseItem.jsx
+++ app/(router)/courses/_components/CourseItem.jsx
@@ -5,19 +5,22 @@ function CourseItem({course}) {
   return (
     <div>
         <Image src={course?.banner?.url} 
-        width={400}
-        height={100}
+        width={200}
+        height={200}
         alt='banner'
-        className='rounded-xl'/>
+        className='rounded-xl mt-2'/>
         <div>
             <h2 className='font-medium'>{course.name}</h2>
             <h2 className='text-[16px] text-gray-400'>{course.author}</h2>
-            <div>
+            <div className='flex gap-2'>
                 <Image src="/youtube.png " 
                 alt='youtube'
                 width={20}
-                height={20}/>
+                height={20}
+                className='hover:animate-bounce'/>
+               <h2 className='text-[14px] text-gray-400' > Watch on YouTube </h2>
             </div>
+            <h2>{ course?.free? 'Free':'Paid' }</h2>
 
         </div>
     </div>
