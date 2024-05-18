d45f666f048bef66e3d2dadcca3522610aad308c
raad <raadbouks@gmail.com>
2024-05-18 23:45:59 +0000

    Update

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -4,15 +4,16 @@ import React from "react";
 
 export default function Page() {
   return (
-    <section className="bg-white">
+ 
+
+<section className="bg-white">
   <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
     <section className="relative flex h-32 items-end bg-gray-900 lg:col-span-5 lg:h-full xl:col-span-6">
       <Image
+      width={600}
+      height={1000}
         alt=""
         src="/login.jpg"
-        width={600}
-        height={1000}
-        className="absolute inset-0 h-full w-full object-cover opacity-80"
       />
 
       <div className="hidden lg:relative lg:block lg:p-12">
@@ -66,18 +67,17 @@ export default function Page() {
           </a>
 
           <h1 className="mt-2 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
-            Welcome to Legendary Innovation 🦑
+            Welcome to Squid 🦑
           </h1>
 
-          <p className="mt-4 leading-relaxed text-gray-500 md:hidden sm:hidden">
+          <p className="mt-4 leading-relaxed text-gray-500">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi nam dolorum aliquam,
             quibusdam aperiam voluptatum.
           </p>
         </div>
 
-       
-
-        <SignIn/>      </div>
+<SignIn />
+      </div>
     </main>
   </div>
 </section>);
--- next.config.mjs
+++ next.config.mjs
@@ -11,4 +11,6 @@ const nextConfig = {
     },
 };
 
+
+
 export default nextConfig;
